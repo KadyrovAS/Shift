7dfd2c0fb774b8a238eb20cc7691f68b8410e69a
Андрей <kadyrovas@yandex.ru>
2024-12-19 01:08:34 +0300

    First commit

---
+++ src/main/java/org/example/Shift.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Shift {
+    public static void main(String[] args) {
+        for (String arg: args)
+        {
+            System.out.println(arg);
+        }
+    }
+}
