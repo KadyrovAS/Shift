29352abadfcfcfe30932c055e6335d19dfc64636
Андрей <kadyrovas@yandex.ru>
2024-12-19 21:47:31 +0300

    First commit

--- src/main/java/org/example/Shift.java
+++ src/main/java/org/example/Shift.java
@@ -1,10 +1,36 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Shift {
     public static void main(String[] args) {
         for (String arg: args)
         {
-            System.out.println(arg);
+             readFromFile(arg);
         }
     }
+
+
+
+    //Читает файл и формирует output
+    public static void readFromFile(String fileName)
+    {
+        try(FileReader reader = new FileReader(fileName))
+        {
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while (bufferedReader.ready())
+            {
+                line = bufferedReader.readLine();
+                System.out.println(line);
+            }
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Error read " + fileName);
+        }
+    }
+
 }
